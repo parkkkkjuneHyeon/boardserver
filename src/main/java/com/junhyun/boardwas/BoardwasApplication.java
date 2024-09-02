2a820fa534fe0d33ca31a5e6dc58225c6d9c7022
박준현 <gktkwk91@naver.com>
2024-09-03 00:42:10 +0900

    project setting

---
+++ src/main/java/com/junhyun/boardwas/BoardwasApplication.java
@@ -0,0 +1,13 @@
+package com.junhyun.boardwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardwasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardwasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junhyun/boardwas/BoardwasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhyun.boardwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardwasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
