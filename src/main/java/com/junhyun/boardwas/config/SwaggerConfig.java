234a9660f0320cdcd0261a02eab7c307f7398b5c
parkkkkjuneHyeon <gktkwk91@naver.com>
2025-06-23 14:21:27 +0900

    feat: swagger 기능 추가

---
+++ src/main/java/com/junhyun/boardwas/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.junhyun.boardwas.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Board Was")
+                        .version("v1")
+                        .description("Board Was")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("cookieAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("cookieAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.COOKIE)
+                                .name("JSESSIONID")));
+    }
+}
